11ff277ded0893dc653ec7d26b65f6e82f6d1d82
Digesh Gabel <digeshgabel786@gmail.com>
2025-07-04 12:14:23 +0530

    Add File

---
+++ Delete_the_Middle_Node_of_a_Linked_List.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>  
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (!head || !head->next)
+            return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        prev->next = slow->next;
+        delete slow;
+
+        return head;
+    }
+};
+
+ListNode* createList(const vector<int>& vals) {
+    if (vals.empty()) return nullptr;
+    ListNode* head = new ListNode(vals[0]);
+    ListNode* current = head;
+    for (size_t i = 1; i < vals.size(); ++i) {
+        current->next = new ListNode(vals[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+void deleteList(ListNode* head) {
+    while (head) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+int main() {
+    vector<int> vals = {1, 2, 3, 4, 5};
+    ListNode* head = createList(vals);
+
+    cout << "Original list: ";
+    printList(head);
+
+    Solution sol;
+    head = sol.deleteMiddle(head);
+
+    cout << "After deleting middle node: ";
+    printList(head);
+
+    deleteList(head);
+
+    return 0;
+}
