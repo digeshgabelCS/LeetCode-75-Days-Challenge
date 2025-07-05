fc9245ad61354e289e1d47bb582fcc345a93bf96
Digesh Gabel <digeshgabel786@gmail.com>
2025-07-05 10:26:42 +0530

    Add File

---
+++ Odd_Even_Linked_List.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+
+            even->next = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead;
+        return head;
+    }
+};
+
+ListNode* createList(int arr[], int size) {
+    if (size == 0) return nullptr;
+
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < size; ++i) {
+        curr->next = new ListNode(arr[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    ListNode* head = createList(arr, size);
+
+    cout << "Original list: ";
+    printList(head);
+
+    Solution solution;
+    ListNode* result = solution.oddEvenList(head);
+
+    cout << "After reordering: ";
+    printList(result);
+
+    return 0;
+}
