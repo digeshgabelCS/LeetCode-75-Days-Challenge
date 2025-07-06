31c55eb0e99f47f37218d24d55c3872581a83be8
Digesh Gabel <digeshgabel786@gmail.com>
2025-07-06 10:22:22 +0530

    Add File

---
+++ Reverse_Linked_List.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next; 
+            curr->next = prev;               
+            prev = curr;                     
+            curr = nextTemp;                 
+        }
+
+        return prev; 
+    }
+};
+
+ListNode* createList(int arr[], int size) {
+    if (size == 0) return nullptr;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < size; ++i) {
+        curr->next = new ListNode(arr[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+void printList(ListNode* head) {
+    ListNode* curr = head;
+    while (curr != nullptr) {
+        cout << curr->val;
+        if (curr->next != nullptr) cout << " -> ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+void deleteList(ListNode* head) {
+    while (head != nullptr) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    ListNode* head = createList(arr, size);
+
+    cout << "Original list: ";
+    printList(head);
+
+    Solution sol;
+    ListNode* reversedHead = sol.reverseList(head);
+
+    cout << "Reversed list: ";
+    printList(reversedHead);
+
+    deleteList(reversedHead);
+
+    return 0;
+}
