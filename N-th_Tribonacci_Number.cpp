9caf669802551dc0c381d1e1e2580cb5a362d55e
Digesh Gabel <digeshgabel786@gmail.com>
2025-08-03 10:16:05 +0530

    Add File

---
+++ N-th_Tribonacci_Number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int t0 = 0, t1 = 1, t2 = 1, t3;
+        for (int i = 3; i <= n; ++i) {
+            t3 = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = t3;
+        }
+        return t2;
+    }
+};
+
+int main() {
+    Solution sol;
+    int n;
+
+    cout << "Enter value of n: ";
+    cin >> n;
+
+    int result = sol.tribonacci(n);
+    cout << "Tribonacci number T" << n << " = " << result << endl;
+
+    return 0;
+}
