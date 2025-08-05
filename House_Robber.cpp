acef9a3914849ae3217d2912114464d1d04dd82b
Digesh Gabel <digeshgabel786@gmail.com>
2025-08-05 23:25:35 +0530

    Add File

---
+++ House_Robber.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        
+        for (int i = 2; i < n; ++i) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        
+        return dp[n - 1];
+    }
+};
+
+int main() {
+    Solution solution;
+    int n;
+    cout << "Enter number of houses: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter money in each house:\n";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    int maxLoot = solution.rob(nums);
+    cout << "Maximum money that can be robbed: " << maxLoot << endl;
+
+    return 0;
+}
