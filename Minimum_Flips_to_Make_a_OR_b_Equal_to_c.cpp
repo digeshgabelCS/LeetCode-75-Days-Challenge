6ef20bc5bcd8495e41dea5187fcd9246615d9c04
Digesh Gabel <digeshgabel786@gmail.com>
2025-08-13 21:29:23 +0530

    Add File

--- Minimum_Flips_to_Make_a_OR_b_Equal_to_c.cpp
+++ Minimum_Flips_to_Make_a_OR_b_Equal_to_c.cpp
@@ -5,23 +5,36 @@ class Solution {
 public:
     int minFlips(int a, int b, int c) {
         int flips = 0;
-        for (int i = 0; i < 32; i++) {
+        
+        for (int i = 0; i < 32; ++i) {
             int bitA = (a >> i) & 1;
             int bitB = (b >> i) & 1;
             int bitC = (c >> i) & 1;
-
+            
+            if ((bitA | bitB) == bitC) {
+                continue;
+            }
+            
             if (bitC == 1) {
-                if ((bitA | bitB) == 0) {
-                    flips += 1; 
-                }
+                flips += 1;
             } else {
-                if (bitA == 1 && bitB == 1) {
-                    flips += 2; 
-                } else if (bitA == 1 || bitB == 1) {
-                    flips += 1; 
-                }
+                flips += bitA + bitB; 
             }
         }
+        
         return flips;
     }
 };
+
+int main() {
+    Solution sol;
+    int a, b, c;
+    
+    cout << "Enter values of a, b, c: ";
+    cin >> a >> b >> c;
+    
+    int result = sol.minFlips(a, b, c);
+    cout << "Minimum flips required: " << result << endl;
+    
+    return 0;
+}
